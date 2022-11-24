ad0fb525740d21e14320f02b3eb9261ef9fb17c2
Shigma <shigma10826@gmail.com>
2022-11-25 00:14:12 +0800

    feat(core): setup core package

---
+++ packages/core/src/index.ts
@@ -0,0 +1,72 @@
+import { Context, Schema, segment, Service } from 'koishi'
+import { createHash } from 'crypto'
+import { basename } from 'path'
+import FileType from 'file-type'
+
+Context.service('assets')
+
+abstract class Assets<C extends Assets.Config = Assets.Config> extends Service {
+  static types = ['image', 'audio', 'video']
+  protected types: readonly string[] = Assets.types
+
+  abstract upload(url: string, file: string): Promise<string>
+  abstract stats(): Promise<Assets.Stats>
+
+  constructor(protected ctx: Context, public config: C) {
+    super(ctx, 'assets')
+  }
+
+  public async transform(content: string) {
+    return await segment.transformAsync(content, Object.fromEntries(this.types.map((type) => {
+      return [type, async (data) => {
+        if (this.config.whitelist.some(prefix => data.url.startsWith(prefix))) {
+          return segment(type, data)
+        } else {
+          return segment(type, { url: await this.upload(data.url, data.file) })
+        }
+      }]
+    })))
+  }
+
+  protected async analyze(url: string, name = ''): Promise<Assets.FileInfo> {
+    const file = await this.ctx.http.file(url)
+    const buffer = Buffer.from(file.data)
+    const hash = createHash('sha1').update(buffer).digest('hex')
+    if (name) {
+      name = basename(name)
+      if (!name.startsWith('.')) {
+        name = `-${name}`
+      }
+    } else {
+      const fileType = await FileType.fromBuffer(buffer)
+      if (fileType) {
+        name = `.${fileType.ext}`
+      }
+    }
+    return { buffer, hash, name, filename: `${hash}${name}` }
+  }
+}
+
+namespace Assets {
+  export interface Stats {
+    assetCount?: number
+    assetSize?: number
+  }
+
+  export interface FileInfo {
+    buffer: Buffer
+    hash: string
+    name: string
+    filename: string
+  }
+
+  export interface Config {
+    whitelist?: string[]
+  }
+
+  export const Config: Schema<Config> = Schema.object({
+    whitelist: Schema.array(Schema.string().required().role('link')).description('不处理的白名单 URL 列表。'),
+  })
+}
+
+export default Assets
