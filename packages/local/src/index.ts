63670b02bbec63efacde36ad15c4eeb2c6cd729a
Shigma <shigma10826@gmail.com>
2023-01-07 01:52:52 +0800

    feat(local): support file url fallback

--- packages/local/src/index.ts
+++ packages/local/src/index.ts
@@ -1,12 +1,14 @@
-import { Context, sanitize, Schema, trimSlash } from 'koishi'
+import { Context, Logger, sanitize, Schema, trimSlash } from 'koishi'
 import { createReadStream, promises as fs } from 'fs'
 import { basename, resolve } from 'path'
 import { createHmac } from 'crypto'
 import { stream as fileTypeStream } from 'file-type'
 import Assets from '@koishijs/assets'
 
+const logger = new Logger('assets')
+
 class LocalAssets extends Assets<LocalAssets.Config> {
-  private _promise: Promise<void>
+  private _Task: Promise<void>
   private _stats: Assets.Stats = {
     assetCount: 0,
     assetSize: 0,
@@ -19,50 +21,51 @@ class LocalAssets extends Assets<LocalAssets.Config> {
   constructor(ctx: Context, config: LocalAssets.Config) {
     super(ctx, config)
 
-    this.path = sanitize(config.path || '/assets')
-    if (config.root) {
-      this.root = resolve(ctx.baseDir, config.root)
-    } else {
-      this.root = resolve(__dirname, '../public')
-    }
+    this.path = sanitize(config.path || '/files')
+    this.root = resolve(ctx.baseDir, config.root)
 
     if (config.selfUrl) {
       this.selfUrl = trimSlash(config.selfUrl)
     } else if (!(this.selfUrl = ctx.root.config.selfUrl)) {
-      throw new Error(`missing configuration "selfUrl"`)
+      logger.warn('missing configuration "selfUrl", fallback to "file:" scheme')
+      this.path = this.root.replace(/^\//, '')
+      this.selfUrl = 'file:///'
     }
 
-    ctx.router.get(this.path, async (ctx) => {
+    if (config.selfUrl) this.initServer()
+    this._Task = this.initFolder()
+  }
+
+  async initServer() {
+    this.ctx.router.get(this.path, async (ctx) => {
       return ctx.body = await this.stats()
     })
 
-    ctx.router.get(this.path + '/:name', async (ctx) => {
+    this.ctx.router.get(this.path + '/:name', async (ctx) => {
       const filename = resolve(this.root, basename(ctx.params.name))
       const stream = await fileTypeStream(createReadStream(filename))
       ctx.type = stream.fileType?.mime
       return ctx.body = stream
     })
 
-    ctx.router.post(this.path, async (ctx) => {
+    this.ctx.router.post(this.path, async (ctx) => {
       const { salt, sign, url, file } = ctx.query
       if (Array.isArray(file) || Array.isArray(url)) {
         return ctx.status = 400
       }
 
-      if (config.secret) {
+      if (this.config.secret) {
         if (!salt || !sign) return ctx.status = 400
-        const hash = createHmac('sha1', config.secret).update(file + salt).digest('hex')
+        const hash = createHmac('sha1', this.config.secret).update(file + salt).digest('hex')
         if (hash !== sign) return ctx.status = 403
       }
 
       await this.upload(url, file)
       return ctx.status = 200
     })
-
-    this._promise = this.init()
   }
 
-  async init() {
+  async initFolder() {
     await fs.mkdir(this.root, { recursive: true })
     const filenames = await fs.readdir(this.root)
     this._stats.assetCount = filenames.length
@@ -80,7 +83,7 @@ class LocalAssets extends Assets<LocalAssets.Config> {
 
   async upload(url: string, file: string) {
     if (url.startsWith(this.selfUrl)) return url
-    await this._promise
+    await this._Task
     const { selfUrl, path, root } = this
     const { buffer, filename } = await this.analyze(url, file)
     const savePath = resolve(root, filename)
@@ -89,7 +92,7 @@ class LocalAssets extends Assets<LocalAssets.Config> {
   }
 
   async stats() {
-    await this._promise
+    await this._Task
     return this._stats
   }
 }
@@ -104,7 +107,7 @@ namespace LocalAssets {
 
   export const Config: Schema<Config> = Schema.intersect([
     Schema.object({
-      root: Schema.string().description('本地存储资源文件的绝对路径。'),
+      root: Schema.string().default('public').description('本地存储资源文件的相对路径。'),
       path: Schema.string().default('/files').description('静态图片暴露在服务器的路径。'),
       selfUrl: Schema.string().role('link').description('Koishi 服务暴露在公网的地址。缺省时将使用全局配置。'),
       secret: Schema.string().description('用于验证上传者的密钥，配合 assets-remote 使用。').role('secret'),
