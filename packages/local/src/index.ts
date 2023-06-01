f2d945008d52fd558597640120c6d093ba619395
Shigma <shigma10826@gmail.com>
2023-06-02 02:17:34 +0800

    feat(local): migrate to data directory

--- packages/local/src/index.ts
+++ packages/local/src/index.ts
@@ -1,6 +1,7 @@
 import { Context, Logger, sanitize, Schema, trimSlash } from 'koishi'
-import { createReadStream, promises as fs } from 'fs'
-import { basename, resolve } from 'path'
+import { createReadStream, Stats } from 'fs'
+import { copyFile, mkdir, readdir, rm, stat, writeFile } from 'fs/promises'
+import { basename, join, resolve } from 'path'
 import { createHmac } from 'crypto'
 import { stream as fileTypeStream } from 'file-type'
 import Assets from '@koishijs/assets'
@@ -8,7 +9,7 @@ import Assets from '@koishijs/assets'
 const logger = new Logger('assets')
 
 class LocalAssets extends Assets<LocalAssets.Config> {
-  private _Task: Promise<void>
+  private _task: Promise<void>
   private _stats: Assets.Stats = {
     assetCount: 0,
     assetSize: 0,
@@ -35,7 +36,42 @@ class LocalAssets extends Assets<LocalAssets.Config> {
     }
 
     if (!this.noServer) this.initServer()
-    this._Task = this.initFolder()
+  }
+
+  // cp() can only be used since node 16
+  async cp(src: string, dest: string) {
+    const dirents = await readdir(src, { withFileTypes: true })
+    for (const dirent of dirents) {
+      const srcFile = join(src, dirent.name)
+      const destFile = join(dest, dirent.name)
+      if (dirent.isFile()) {
+        await copyFile(srcFile, destFile)
+      } else if (dirent.isDirectory()) {
+        await mkdir(destFile)
+        await this.cp(srcFile, destFile)
+      }
+    }
+  }
+
+  async _start() {
+    const legacy = resolve(this.ctx.baseDir, 'public')
+    await mkdir(this.root, { recursive: true })
+    const stats: Stats = await stat(legacy).catch(() => null)
+    if (stats?.isDirectory()) {
+      logger.info('migrating to data directory')
+      await this.cp(legacy, this.root)
+      await rm(legacy, { recursive: true, force: true })
+    }
+    const filenames = await readdir(this.root)
+    this._stats.assetCount = filenames.length
+    await Promise.all(filenames.map(async (file) => {
+      const { size } = await stat(resolve(this.root, file))
+      this._stats.assetSize += size
+    }))
+  }
+
+  start() {
+    this._task = this._start()
   }
 
   async initServer() {
@@ -66,25 +102,15 @@ class LocalAssets extends Assets<LocalAssets.Config> {
     })
   }
 
-  async initFolder() {
-    await fs.mkdir(this.root, { recursive: true })
-    const filenames = await fs.readdir(this.root)
-    this._stats.assetCount = filenames.length
-    await Promise.all(filenames.map(async (file) => {
-      const { size } = await fs.stat(resolve(this.root, file))
-      this._stats.assetSize += size
-    }))
-  }
-
   async write(buffer: Buffer, filename: string) {
-    await fs.writeFile(filename, buffer)
+    await writeFile(filename, buffer)
     this._stats.assetCount += 1
     this._stats.assetSize += buffer.byteLength
   }
 
   async upload(url: string, file: string) {
     if (url.startsWith(this.selfUrl)) return url
-    await this._Task
+    await this._task
     const { selfUrl, path, root } = this
     const { buffer, filename } = await this.analyze(url, file)
     const savePath = resolve(root, filename)
@@ -93,7 +119,7 @@ class LocalAssets extends Assets<LocalAssets.Config> {
   }
 
   async stats() {
-    await this._Task
+    await this._task
     return this._stats
   }
 }
@@ -108,7 +134,7 @@ namespace LocalAssets {
 
   export const Config: Schema<Config> = Schema.intersect([
     Schema.object({
-      root: Schema.string().default('public').description('本地存储资源文件的相对路径。'),
+      root: Schema.path({ filters: ['directory'] }).default('data/assets').description('本地存储资源文件的相对路径。'),
       path: Schema.string().default('/files').description('静态图片暴露在服务器的路径。'),
       selfUrl: Schema.string().role('link').description('Koishi 服务暴露在公网的地址。缺省时将使用全局配置。'),
       secret: Schema.string().description('用于验证上传者的密钥，配合 assets-remote 使用。').role('secret'),
